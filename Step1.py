3769bc17cfb2a94edadbdb56547082cbdcd8f056
MrChrom <emoki.storage@gmail.com>
2016-04-02 23:59:08 -0400

    moving on, page 6

---
+++ Step1.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+# variable to keep our main loop running
+running = True
+
+# main loop
+while running:
+    # for loop through the even queue
+    for event in pygame.event.get():
+        # check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals
+        if event.type == KEYDOWN:
+            # if the Esc key has been pressed, set running to false to exit the main loop
+            if event.key == K_ESCAPE:
+                running = False
+        # check for QUIT event; if QUIT, set running to false
+        elif event.type == QUIT:
+            running = False
+
+    # Create the surface and pass in a tuple with its length and width
+    surf = pygame.Surface((50, 50))
+    # Give the surface a color to differentiate it from the background
+    surf.fill((255, 255, 255))
+
+    # This line says "Draw surf onto screen at coordinates x:400, y:300"
+    screen.blit(surf, (400, 300))
+    pygame.display.flip()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
--- main.py
+++ main.py
@@ -1,32 +1,48 @@
+# import the pygame module
 import pygame
+
+# import pygame.locals for easier access to key coordinates
 from pygame.locals import *
 
+# Define our player object and call super to give it all the properties and methods of pygame.sprite.Sprite
+pygame.sprite.Sprite
+# The surface we draw on the screen is now a property of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+# initialize pygame
 pygame.init()
 
+# create the screen object
+# here we pass it a size of 800x600
 screen = pygame.display.set_mode((800, 600))
 
-# variable to keep our main loop running
+# instantiate our player; right now he's just a rectangle
+player = Player()
+
+
+# Variable to keep our main loop running
 running = True
 
-# main loop
+# Our main loop!
 while running:
-    # for loop through the even queue
+    # for loop through the event queue
     for event in pygame.event.get():
-        # check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals
+        # Check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals, which we imported earlier
         if event.type == KEYDOWN:
-            # if the Esc key has been pressed, set running to false to exit the main loop
+            # If the Esc key has been pressed set running to false to exit the main loop
             if event.key == K_ESCAPE:
                 running = False
-        # check for QUIT event; if QUIT, set running to false
+        # Check for QUIT event; if QUIT, set running to false
         elif event.type == QUIT:
             running = False
 
-    # Create the surface and pass in a tuple with its length and width
-    surf = pygame.Surface((50, 50))
-    # Give the surface a color to differentiate it from the background
-    surf.fill((255, 255, 255))
-
-    # This line says "Draw surf onto screen at coordinates x:400, y:300"
-    screen.blit(surf, (400, 300))
+    # Draw the player to the screen
+    screen.blit(player.surf, (400, 300))
+    # Update the display
     pygame.display.flip()
-
+    
