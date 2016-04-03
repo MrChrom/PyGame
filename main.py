7bed2d3360759f74480a6f8f65fa5cbf2709c89a
MrChrom <emoki.storage@gmail.com>
2016-04-02 22:16:19 -0400

    This one is better, page 3

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 
 pygame.init()
 
-rect = surf.get_rect()
 screen = pygame.display.set_mode((800, 600))
 
 # variable to keep our main loop running
@@ -22,11 +21,13 @@ while running:
         elif event.type == QUIT:
             running = False
 
-# Create the surface and pass in a tuple with its length and width
-#surf = pygame.Surface((50, 50))
-# Give the surface a color to differentiate it from the background
-#surf.fill((255, 255, 255))
+    # Create the surface and pass in a tuple with its length and width
+    surf = pygame.Surface((50, 50))
+    # Give the surface a color to differentiate it from the background
+    surf.fill((255, 255, 255))
 
-# This line says "Draw surf onto screen at coordinates x:400, y:300"
-#screen.blit(surf, (400, 300))
-#pygame.display.flip()
+    # This line says "Draw surf onto screen at coordinates x:400, y:300"
+    screen.blit(surf, (400, 300))
+    pygame.display.flip()
+
+    
